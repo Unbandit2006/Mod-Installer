a8d50c1eecffd1e0ea8289821149885a9ad80af5
Daniel Zhelenzov <Daniel.Zheleznov@gmail.com>
2023-07-24 14:00:07 -0400

    [07/24/2023] [14:00] The Great Purge
    I am just rewriting the program to better fit everything

--- ModrauderFrame.java
+++ ModrauderFrame.java
@@ -9,6 +9,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 import org.ini4j.*;
@@ -21,6 +23,7 @@ public class ModrauderFrame extends JFrame {
 	JButton installBT;
     String gameVersion;
     int modApi;
+    String modsProvider;
 
     File modsFolder = new File(System.getenv("APPDATA") + "\\.minecraft\\mods");
     boolean delete = false;
@@ -30,14 +33,18 @@ public class ModrauderFrame extends JFrame {
     public ModrauderFrame() {
 
         try {
-            Ini config = new Ini(new File("Config.mc"));
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream("Config.mc");
+            Ini config = new Ini(is);
 
             Section defaults = config.get("Default");
             
             gameVersion = defaults.get("GameVersion", "1.20.1");
 
+            modsProvider = defaults.get("ModsProvider", "Curseforge");
+
             if (defaults.get("ModdingAPI").equals("Forge")) {
                 modApi = 1;
+
             } else if (defaults.get("ModdingAPI").equals("Fabric")) {
                 modApi = 4;
             } else {
@@ -46,10 +53,10 @@ public class ModrauderFrame extends JFrame {
 
             if (defaults.get("ClearPreviousMods").equals("true")) {
                 delete = true;
-            } 
+            }
 
         } catch (IOException e) {
-            log.add("Counldn't access config file");
+            log.add("Couldn't access config file\n"+e.getMessage());
         }
 
 
@@ -57,7 +64,7 @@ public class ModrauderFrame extends JFrame {
 
     public void Render() {
         this.setSize(500, 250);
-        this.setLocation(500, 500);
+        this.setLocation(100, 100);
         this.setTitle("Modrauder Mod Installer - Made By @The Supreme Being");
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -91,11 +98,11 @@ public class ModrauderFrame extends JFrame {
         bar.setVisible(false);
         this.add(bar);
         
-        JButton installBT = new JButton("Install");
+        installBT = new JButton("Install");
         installBT.setFocusable(false);
         installBT.setBackground(Color.GRAY);
         installBT.setForeground(new Color(0xf5f5f5));
-        installBT.setBounds(420, 180, 75, 40);
+        installBT.setBounds(410, 170, 75, 40);
         installBT.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event){
@@ -121,14 +128,16 @@ public class ModrauderFrame extends JFrame {
 
                 bar.setVisible(true);
 
+                if (modsFolder.exists() == false) {
+                    modsFolder.mkdir();
+                }
+
                 if (delete == true) {
                     if (modsFolder.list() != null) {
                         for (File fileName : modsFolder.listFiles()) {
                             fileName.delete();
                         }
                     }
-                    modsFolder.delete();
-                    modsFolder.mkdir();
                     log.add("Deleted all previous mods.");
                     area.setText(String.join("\n", log));
                 }
@@ -144,12 +153,12 @@ public class ModrauderFrame extends JFrame {
                     String modId = modIds.get(i);
                     String modName = modNames.get(i);
 
-                    if (downloadModFiles(modId, gameVersion, modApi, modsFolder)) {
+                    if (downloadModFiles(modId, gameVersion, modApi, modsFolder, modsProvider)) {
                         bar.setValue(bar.getValue()+(int)growth);
-                        log.add(String.format("Found and Installed %s for %s", modName, gameVersion));
+                        log.add(String.format("Found and Installed %s for %s in %s", modName, gameVersion, modsProvider));
                         area.setText(String.join("\n", log));
                     } else {
-                        log.add(String.format("Couldn't find %s for %s", modName, gameVersion));
+                        log.add(String.format("Couldn't find %s for %s in %s", modName, gameVersion, modsProvider));
                         area.setText(String.join("\n", log));
                     }
                 }
@@ -170,7 +179,7 @@ public class ModrauderFrame extends JFrame {
         JLabel myName = new JLabel("Mod Installer Made By: @The Supreme Being");
         myName.setFont(nameFont);
         myName.setForeground(Color.GREEN);
-        myName.setBounds(5, 180, 500, 40);
+        myName.setBounds(5, 170, 500, 40);
         this.add(myName);
 
         this.setLayout(null);
@@ -184,14 +193,14 @@ public class ModrauderFrame extends JFrame {
     public static boolean installFabric(String gameVersion, String minecraftFolder) {
         String downloadLink = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.2/fabric-installer-0.11.2.jar";
         try {
-            
+
             URL downloadUrl = new URL(downloadLink);
             HttpURLConnection connection = (HttpURLConnection) downloadUrl.openConnection();
 
             connection.setRequestMethod("GET");
 
             InputStream inputStream = connection.getInputStream();
-            
+
             FileOutputStream outputStream = new FileOutputStream("fabric_latest.jar");
             byte[] buffer = new byte[4096];
             int bytesRead;
@@ -217,11 +226,13 @@ public class ModrauderFrame extends JFrame {
             ProcessBuilder builder = new ProcessBuilder(commandLine);
             builder = builder.directory(new File("."));
             Process process = builder.start();
-            
+
             process.waitFor();
 
+            new File("fabric_latest.jar").delete();
+
             return true;
-        
+
         } catch (IOException | InterruptedException e) {
             System.out.println(e);
             return false;
@@ -229,11 +240,12 @@ public class ModrauderFrame extends JFrame {
 
     }
 
-    public static ArrayList<String> getModIds() {
+    public ArrayList<String> getModIds() {
         ArrayList<String> modIds = new ArrayList<>();
 
         try {
-            Ini config = new Ini(new File("Config.mc"));
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream("Config.mc");
+            Ini config = new Ini(is);
 
             Section mods = config.get("Mods");
 
@@ -249,11 +261,12 @@ public class ModrauderFrame extends JFrame {
         return modIds;
     }
 
-    public static ArrayList<String> getModNames() {
+    public ArrayList<String> getModNames() {
         ArrayList<String> modNames = new ArrayList<>();
 
         try {
-            Ini config = new Ini(new File("Config.mc"));
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream("Config.mc");
+            Ini config = new Ini(is);
 
             Section mods = config.get("Mods");
 
@@ -268,57 +281,141 @@ public class ModrauderFrame extends JFrame {
         return modNames;
     }    
 
-    public static boolean downloadModFiles(String modId, String gameVersion, int modApi, File modFolder) {
-            URL downloadUrl;
-            try {
-                downloadUrl = new URL(String.format("https://api.curseforge.com/v1/mods/%s/files?gameVersion=%s&modLoaderType=%s&index=0&pageSize=1", modId, gameVersion, modApi));
-            
-                HttpURLConnection connection = (HttpURLConnection) downloadUrl.openConnection();
+    public static boolean downloadModFiles(String modId, String gameVersion, int modApi, File modFolder, String modsProvider) {
+            URL downloadUrl = null;
+            if (modsProvider.equals("Curseforge")) {
+                try {
+                    downloadUrl = new URL(String.format("https://api.curseforge.com/v1/mods/%s/files?gameVersion=%s&modLoaderType=%s&index=0&pageSize=1", modId, gameVersion, modApi));
 
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Accept", "application/json");
-                connection.setRequestProperty("x-api-key", "$2a$10$cxwv/oii490ru.VvB5Tp2OEU2WOO0li0cEVdhEOpIwfAI950iWg/q");
+                    HttpURLConnection connection = (HttpURLConnection) downloadUrl.openConnection();
 
-                InputStream inputStream = connection.getInputStream();
-                
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                StringBuilder response = new StringBuilder();
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("Accept", "application/json");
+                    connection.setRequestProperty("x-api-key", "$2a$10$cxwv/oii490ru.VvB5Tp2OEU2WOO0li0cEVdhEOpIwfAI950iWg/q");
 
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
+                    InputStream inputStream = connection.getInputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    StringBuilder response = new StringBuilder();
+
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+
+                    String downloadLink = JsonParser.parseString(response.toString()).getAsJsonObject().get("data").getAsJsonArray().get(0).getAsJsonObject().get("downloadUrl").getAsString();
+                    String filename = JsonParser.parseString(response.toString()).getAsJsonObject().get("data").getAsJsonArray().get(0).getAsJsonObject().get("fileName").getAsString();
+
+                    inputStream.close();
+
+
+                    // // pt2
+                    URL fileLink = new URL(downloadLink);
+                    connection = (HttpURLConnection) fileLink.openConnection();
+                    connection.setRequestMethod("GET");
+
+                    FileOutputStream outputStream = new FileOutputStream(modFolder.getAbsolutePath() + "\\" + filename);
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+
+                    while ((bytesRead = connection.getInputStream().read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+
+                    outputStream.close();
+                    connection.disconnect();
+
+                    return true;
+
+                } catch (IOException error) {
+                    System.err.println(error);
+                    return false;
                 }
-                reader.close();
+            } else if (modsProvider.equals("Modrinth")) {
+                try {
+                    if (modApi == 1) {
+                        // forge
+                    } else if (modApi == 4) {
+                        // fabric
+                        String baseUrl = "https://api.modrinth.com/v2/project/"+modId+"/version";
+                        String query = "game_versions=[\""+gameVersion+"\"]";
+                        String query1 = "loaders=[\"fabric\"]";
 
-                String downloadLink = JsonParser.parseString(response.toString()).getAsJsonObject().get("data").getAsJsonArray().get(0).getAsJsonObject().get("downloadUrl").getAsString();
-                String filename = JsonParser.parseString(response.toString()).getAsJsonObject().get("data").getAsJsonArray().get(0).getAsJsonObject().get("fileName").getAsString();
+                        String encodedQuery = URLEncoder.encode(query, "UTF-8");
+                        String encodedQuery1 = URLEncoder.encode(query1, "UTF-8");
 
-                inputStream.close();
-            
-            
-                // // pt2
-                URL fileLink = new URL(downloadLink);
-                connection = (HttpURLConnection) fileLink.openConnection();
-                connection.setRequestMethod("GET");
+                        String url = baseUrl+"?"+query+"&"+query1;
+                        downloadUrl = new URL(url);
 
-                FileOutputStream outputStream = new FileOutputStream(modFolder.getAbsolutePath()+"\\"+filename);
-                byte[] buffer = new byte[4096];
-                int bytesRead;
+                        HttpURLConnection connection = (HttpURLConnection) downloadUrl.openConnection();
+                        connection.setRequestMethod("GET");
+                        connection.setRequestProperty("User-Agent", "unbandit2006/mod_installer");
+                        connection.setRequestProperty("Accept", "application/json");
 
-                while ((bytesRead = connection.getInputStream().read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        String line;
+                        StringBuilder response = new StringBuilder();
+
+                        while ((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                        reader.close();
+
+                        System.out.println(JsonParser.parseString(response.toString()));
+
+                        connection.disconnect();
+                    }
+                    // TODO fixme
+
+//                    HttpURLConnection connection = (HttpURLConnection) downloadUrl.openConnection();
+//
+//                    connection.setRequestMethod("GET");
+//                    connection.setRequestProperty("User-Agent", "unbandit2006/mod_installer");
+//
+//                    InputStream inputStream = connection.getInputStream();
+//
+//                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//                    String line;
+//                    StringBuilder response = new StringBuilder();
+//
+//                    while ((line = reader.readLine()) != null) {
+//                        response.append(line);
+//                    }
+//                    reader.close();
+//
+//                    String downloadLink = JsonParser.parseString(response.toString()).getAsJsonArray().get(0).getAsJsonObject().get("files").getAsJsonArray().get(0).getAsJsonObject().get("url").getAsString();
+//                    String filename = JsonParser.parseString(response.toString()).getAsJsonArray().get(0).getAsJsonObject().get("files").getAsJsonArray().get(0).getAsJsonObject().get("filename").getAsString();
+//
+//                    inputStream.close();
+//
+//                    // pt2
+//                    URL fileLink = new URL(downloadLink);
+//                    connection = (HttpURLConnection) fileLink.openConnection();
+//                    connection.setRequestMethod("GET");
+//
+//                    System.out.println(connection.getOutputStream());
+
+//                    FileOutputStream outputStream = new FileOutputStream(modFolder.getAbsolutePath() + "\\" + filename);
+//                    byte[] buffer = new byte[4096];
+//                    int bytesRead;
+//
+//                    while ((bytesRead = connection.getInputStream().read(buffer)) != -1) {
+//                        outputStream.write(buffer, 0, bytesRead);
+//                    }
+//
+//                    outputStream.close();
+
+                    return true;
+
+                } catch (IOException error) {
+                    System.err.println(error);
+                    return false;
                 }
+            } else {
+                return false;
+            }
 
-                outputStream.close();
-                connection.disconnect();
-
-            return true;
-
-        } catch (IOException error) {
-            System.err.println(error);
-            return false;
-        }
-        
     }
 
 }
